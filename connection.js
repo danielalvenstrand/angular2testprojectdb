ad8433dcde858bcdef0b6d35b4cbb911345542ed
danielalvenstrand <daniel.alvenstrand@capgemini.com>
2016-08-02 10:25:31 +0200

    Fixed timeout and errors

--- connection.js
+++ connection.js
@@ -8,13 +8,15 @@ function Connection() {
       host     : 'alvenstranddb.cs5al0wf1hwt.eu-west-1.rds.amazonaws.com',
       user     : 'dalvenst',
       password : 'LsDa9291',
-      database : 'angular2testprojectdb'
+      database : 'angular2testprojectdb',
+      acquireTimeout: 30000
     });
   };
 
   this.acquire = (callback) => {
     this.pool.getConnection((err, connection) => {
-      callback(err, connection);
+      if (err) console.log(err);
+      else callback(err, connection);
     });
   };
 }
